26aac7c9ec821d4253ab23e1403db9b6544671ae
MaxDuke <maxduke@loopop.net>
2014-06-22 14:28:36 +0800

    added a test class

---
+++ DXTest.h
@@ -0,0 +1,13 @@
+//
+//  DXTest.h
+//  Git Test
+//
+//  Created by MaxDuke on 6/22/14.
+//  Copyright (c) 2014 DreamSpace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DXTest : NSObject
+
+@end
---
+++ DXTest.m
@@ -0,0 +1,13 @@
+//
+//  DXTest.m
+//  Git Test
+//
+//  Created by MaxDuke on 6/22/14.
+//  Copyright (c) 2014 DreamSpace. All rights reserved.
+//
+
+#import "DXTest.h"
+
+@implementation DXTest
+
+@end
